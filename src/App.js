24f63cd2cb67f001064aab4854dab40605328ecd
Filip <sieniu212@gmail.com>
2021-08-21 14:28:49 +0200

    04

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DishPage from "./components/DishPage";
 import LetterNav from "./components/LetterNav";
+import Header from "./components/Header";
 
 import AppProvider from "./Context/AppContext";
 
@@ -17,6 +18,7 @@ const App = () => {
 
   return (
     <div className="app">
+      <Header />
       <AppProvider>
         <div style={style}>
           <LetterNav />
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div></div>
+      <h1>Moja aplikacja kulinarna</h1>
     </header>
   );
 };
