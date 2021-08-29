47b8149c3ca7fa183121b31f3ae7d71d36c686b0
Filip <sieniu212@gmail.com>
2021-08-29 17:38:08 +0200

    06

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,14 @@ const Card = (props) => {
   console.log(props);
   return (
     <div className="dishCard" id={props.dish.idMeal}>
-      <h1>{props.dish.strMeal}</h1>
+      <img src={props.dish.strMealThumb} alt={props.dish.strMealThumb} />
+
+      <div className="dishTitle">
+        <h1>{props.dish.strMeal}</h1>
+      </div>
+      <div className="text">
+        <p></p>
+      </div>
     </div>
   );
 };
